021068e786aad35caffd58206d00b64a692cf646
jahnavi-adabala <jahnavi.adabala@sasi.ac.in>
2025-10-10 22:01:51 +0530

    Initial SpringWork

---
+++ MyApp/src/main/java/com/diaries/MyApp/Dev.java
@@ -0,0 +1,11 @@
+package com.diaries.MyApp;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on a Project");
+    }
+}
